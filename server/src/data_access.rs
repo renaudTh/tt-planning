06ef7133c261d6bb393f65be150330e629c0a253
Thomas Renaud <t.renaud@neuf.fr>
2023-02-16 14:32:32 +0100

    Rewrite select distinct names with rusqlite

--- server/src/data_access.rs
+++ server/src/data_access.rs
@@ -1,13 +1,44 @@
+
+use rusqlite::{Connection, Result};
 use super::models::*;
 
+pub struct Dao{
+    connection: Connection
+}
+impl Dao{
+    pub fn new(path:&str) -> Dao{
+        
+        Dao{
+            connection: Connection::open(path).unwrap()
+        }
+    }
 
+    fn get_distinct_names(&self, poll_id:i64) -> Result<Vec<String>>{
+        let mut ret : Vec<String> = Vec::new();
+        let mut statement = self.connection.prepare("SELECT DISTINCT(name) FROM Vote WHERE poll_id = ?")?;
+        let mut result_iter = statement.query([poll_id])?;
 
+        while let Some(row) = result_iter.next()? {
+            let value = row.get(0).unwrap();
+            ret.push(value);
+        }
+        Ok(ret)
+       
+    }   
+   
+}
 #[cfg(test)]    
 mod tests {
     use super::*;
 
     #[test]
     fn get_distinct_names_test() {
-        assert_eq!(1+1,2);
+        
+        let dao = Dao::new("../db.sqlite3");
+        let res = dao.get_distinct_names(1).unwrap();
+        println!("{:?}", res);
+        assert!(res.contains(&String::from("Simon")));
+        assert!(res.contains(&String::from("Thomas")));
+        assert_eq!(res.len(),3);
     }
 }
